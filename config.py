48395dd204a0bd6f3c224bc8e383f8e0c47e7f4f
Oh DoYoeon <odyody12@gmail.com>
2014-10-06 21:59:25 +0900

    part5. User Logins

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app2.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository2')
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'http://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenId', 'url': 'https://www.myopenid.com'}
+]
+
+
